8b080d66631c5226540d92ca5a67f20696e12b65
Fernando Monteiro <fernandoflmbatera@gmail.com>
2022-04-26 11:47:57 -0300

    Os Pokemons são listados na p

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,8 +1,51 @@
-import React from 'react';
+import axios from 'axios';
+import React, { useEffect, useState } from 'react';
 
 const Home: React.FC = () => {
+  const [data, setData] = useState<any>({
+    results: [
+      {
+        name: 'bulbasaur',
+        url: 'https://pokeapi.co/api/v2/pokemon/1/',
+      },
+      {
+        name: 'ivysaur',
+        url: 'https://pokeapi.co/api/v2/pokemon/2/',
+      }
+    ]
+  });
+
+  const fetch = async (url: string) => {
+    const response = await axios({
+      method: 'GET',
+      url,
+    });
+
+    return response.data;
+  };
+
+  const loadPokemons = async () => {
+    const pokeData = await fetch('https://pokeapi.co/api/v2/pokemon/?offset=0&limit=9999');
+    await setData(pokeData);
+  };
+
+  useEffect(() => { loadPokemons() }, []);
+
   return (
-    <div>test1</div>
+    <>
+      {data.results.map((poke: any, i: number) => {
+        const pokedex = poke.url.slice(34, -1);
+        return (
+          <div>
+            <img
+              src={`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${pokedex}.png`}
+              alt={poke.name}
+            />
+            <span key={i}>{poke.name}</span>
+          </div>
+        );
+      })}
+    </>
   );
 };
 
