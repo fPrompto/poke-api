636f580047dc8b739853d08eb0508d10a2f4d751
Fernando Monteiro <fernandoflmbatera@gmail.com>
2022-04-27 12:48:47 -0300

    Alteracoes no CSS da pagina de detalhes

--- src/pages/PokeDetails.tsx
+++ src/pages/PokeDetails.tsx
@@ -1,4 +1,4 @@
-import React, { PropsWithChildren, useEffect, useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import { useParams } from 'react-router-dom';
 import Loading from '../components/Loading';
 
@@ -6,6 +6,8 @@ import { Heading, Image } from '@chakra-ui/react'
 
 import { fetch } from '../utils/fetch';
 import { firstCharUpperCase } from '../utils/firstCharUpperCase';
+
+import '../css/PokeDetails.css';
 
 const PokeDetails: React.FC = () => {
   const [data, setData] = useState<any>('');
@@ -27,69 +29,76 @@ const PokeDetails: React.FC = () => {
     <>
       <Heading as='h1' size='4xl'>{firstCharUpperCase(data.name)}</Heading>
 
-      <Image
-        src={`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${id}.png`}
-        alt=""
-        boxSize='400px'
-        objectFit='cover'
-      />
-      <Image
-        src={`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/shiny/${id}.png`}
-        alt=""
-      />
+      <div className='images'>
+        <Image
+          src={`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${id}.png`}
+          alt=""
+          boxSize='400px'
+          objectFit='cover'
+        />
+        <Image
+          src={`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/shiny/${id}.png`}
+          alt=""
+          boxSize='400px'
+          objectFit='cover'
+        />
 
-      {/* sprites - back */}
-      <Image
-        src={`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/back/${id}.png`}
-        alt=""
-      />
-      <Image
-        src={`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/back/shiny/${id}.png`}
-        alt=""
-      />
+        {/* sprites - back */}
+        <Image
+          src={`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/back/${id}.png`}
+          alt=""
+        />
+        <Image
+          src={`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/back/shiny/${id}.png`}
+          alt=""
+        />
+      </div>
 
       <br />
 
-      <div className='status'>
-        <Heading as='h2' size='3xl'>Status</Heading>
-        <br />
-        {data.stats.map((stat: any, i: number) => {
-          const newName = firstCharUpperCase(stat.stat.name);
-          return (
-            <>
-              <span key={i}>{`${newName}: `}</span>
-              <span key={i}>{`${stat.base_stat} + Effort: ${stat.effort}`}</span>
-              <br />
-            </>
-          );
-        })}
-        <span>{`Height: ${data.height}`}</span>
-        <br />
-        <span>{`Weight: ${data.weight}`}</span>
-        <br />
-        <span>{`Base experience: ${data.base_experience}`}</span>
-      </div>
 
-      <div className='abilities'>
-        <Heading as='h2' size='3xl'>Abilities</Heading>
-        <br />
-        {data.abilities.map((abilit: any, i: number) => {
-          const newAbility = abilit.ability.is_hidden ? 'Yes' : 'No';
-          return (
-            <>
-              <span>{`Name: ${firstCharUpperCase(abilit.ability.name)}`}</span>
-              <br />
-              <span>{`Hidden: ${newAbility}`}</span>
-              <br />
-              <span>{`Slot: ${abilit.slot}`}</span>
-              <br />
-            </>
-          );
-        })}
+      <div className='status-abilities-group'>
+        <div className='status'>
+          <Heading as='h2' size='2xl'>Status</Heading>
+          <br />
+          {data.stats.map((stat: any, i: number) => {
+            const newName = firstCharUpperCase(stat.stat.name);
+            return (
+              <>
+                <span key={i}>{`${newName}: `}</span>
+                <span key={i}>{`${stat.base_stat} + Effort: ${stat.effort}`}</span>
+                <br />
+              </>
+            );
+          })}
+          <span>{`Height: ${data.height}`}</span>
+          <br />
+          <span>{`Weight: ${data.weight}`}</span>
+          <br />
+          <span>{`Base experience: ${data.base_experience}`}</span>
+        </div>
+
+        <div className='abilities'>
+          <Heading as='h2' size='2xl'>Abilities</Heading>
+          <br />
+          {data.abilities.map((abilit: any, i: number) => {
+            const newAbility = abilit.ability.is_hidden ? 'Yes' : 'No';
+            return (
+              <>
+                <span>{`Name: ${firstCharUpperCase(abilit.ability.name)}`}</span>
+                <br />
+                <span>{`Hidden: ${newAbility}`}</span>
+                <br />
+                <span>{`Slot: ${abilit.slot}`}</span>
+                <br />
+              </>
+            );
+          })}
+        </div>
       </div>
 
       <div className='moves'>
-        <Heading as='h2' size='3xl'>Moves</Heading>
+        <Heading as='h2' size='2xl'>Moves</Heading>
         <br />
         {data.moves.map((mov: any, i: number) => {
           const newName = firstCharUpperCase(mov.move.name);
@@ -98,13 +107,12 @@ const PokeDetails: React.FC = () => {
               const newM = firstCharUpperCase(method.move_learn_method.name);
               const newV = firstCharUpperCase(method.version_group.name);
               return (
-                <div className='move-name'>
+                <div className='version-name'>
                   <span>{`Move learn method: ${newM}`}</span>
                   <br />
                   <span>{`Version group: ${newV}`}</span>
                   <br />
                   <span>{`Level learned at: ${method.level_learned_at}`}</span>
-                  <br />
                   <br />
                 </div>
 
@@ -116,7 +124,9 @@ const PokeDetails: React.FC = () => {
               <br />
               <Heading as='h4' size='md'>Version group Details</Heading>
               <br />
-              {mapVersionGroup}
+              <div className='version-group'>
+                {mapVersionGroup}
+              </div>
               <br />
             </div>
           );
